e3c2b2098584769511865f4fd8efe8496df5614f
ViktorSemikolenov <35874330+ViktorSemikolenov@users.noreply.github.com>
2023-01-13 16:49:30 +0600

    Add files via upload

---
+++ OUTDIGITAL/script/main.js
@@ -0,0 +1,43 @@
+new WOW().init();
+
+// // Check for hash value in URL
+// var hash = window.location.hash.substr(1);
+// var href = $('.btn').each(function(){
+//     var href = $(this).attr('href');
+//     if(hash==href.substr(0,href.length-5)){
+//         var toLoad = hash+'.html #content';
+//         $('#content').load(toLoad)
+//     } 
+// });
+// // Check for hash value in URL
+// var hash = window.location.hash.substr(1);
+// var href = $('.btn').each(function(){
+//     var href = $(this).attr('href');
+//     if(hash==href.substr(0,href.length-5)){
+//         var toLoad = hash+'.html #content';
+//         $('#content').load(toLoad)
+//     } 
+// });
+
+// $('.btn').hover(function(){
+
+// var toLoad = $(this).attr('href')+' #content';
+// $('#content').hide('fast',loadContent);
+// $('#load').remove();
+// $('#wrapper').append('<span id="load">LOADING...</span>');
+// $('#load').fadeIn('normal');
+// window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-0);
+// function loadContent() {
+//     $('#content').load(toLoad,'',showNewContent())
+// }
+// function showNewContent() {
+//     $('#content').show('normal',hideLoader());
+// }
+// function hideLoader() {
+//     $('#load').fadeIn('normal');
+// }
+// return false;
+
+// });
+
+
