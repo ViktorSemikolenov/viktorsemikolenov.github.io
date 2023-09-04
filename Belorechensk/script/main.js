3c9eb3ca01cfa5408a7a5c9d8c4f5f31d76cd56a
Viktor Semikolenov <viktor.semikolenov@gmail.com>
2023-09-04 13:24:51 +0600

    new-project

--- Belorechensk/script/main.js
+++ Belorechensk/script/main.js
@@ -1,29 +1,108 @@
 function formatDate(date) {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString('ru-RU', options);
-    
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    if (hours < 10) {
-      hours = '0' + hours;
-    }
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-    
-    const formattedTime = hours + ':' + minutes;
-    
-    return formattedDate + ' (' + formattedTime + ')';
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  const formattedDate = date.toLocaleDateString('ru-RU', options);
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = '0' + hours;
   }
-  
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+
+  const formattedTime = hours + ':' + minutes;
+
+  return formattedDate + ' (' + formattedTime + ')';
+}
+
+$(document).ready(function() {
   const currentDate = new Date();
-  const timerPage = document.querySelector('.timer-page');
-  timerPage.textContent = formatDate(currentDate);
+  const timerPage = $('.timer-page');
+  timerPage.text(formatDate(currentDate));
 
   function updateTime() {
     const currentDate = new Date();
     const formattedDate = formatDate(currentDate);
-    timerPage.textContent = formattedDate;
+    timerPage.text(formattedDate);
   }
-  
+
   setInterval(updateTime, 1000);
+});
+
+// ---------------------------------------------
+
+$(window).scroll(function() {
+  var navigation = $(".navigation");
+  var scrollPosition = $(window).scrollTop();
+
+  if (scrollPosition >= 194) {
+    navigation.addClass("fixed");
+  } else {
+    navigation.removeClass("fixed");
+  }
+});
+
+// ----------------------------------------------------------
+
+$(document).ready(function() {
+  $('.search-popup').click(function() {
+    $('.search-block').addClass('show');
+    $('body').css('overflow', 'hidden');
+  });
+
+  $('.close-search, .search-block .overflow').click(function() {
+    $('.search-block').removeClass('show');
+    $('body').css('overflow', 'visible');
+  });
+});
+
+// ------------------------------------------------------------
+
+$(function() {
+  $('.tabs-nav a').click(function() {
+
+    // Check for active
+    $('.tabs-nav li').removeClass('active');
+    $(this).parent().addClass('active');
+
+    // Display active tab
+    let currentTab = $(this).attr('href');
+    $('.tabs-content div').hide();
+    $(currentTab).fadeIn(300);
+
+    return false;
+  });
+});
+
+// -------------------------------------------------
+$(document).ready(function() {
+  const $sitesCity = $('.sites-city');
+  
+  $('.all-sites').click(function() {
+    $sitesCity.fadeIn(300);
+    $('body').css('overflow', 'hidden');
+  });
+
+  $sitesCity.find('.close').click(function() {
+    $sitesCity.fadeOut(300);
+    $('body').css('overflow', 'visible');
+  });
+});
+// ----------------------------
+$('.drop-navigation__header').click(function() {
+  var parentCol = $(this).parent('.drop-navigation__col');
+  parentCol.toggleClass('active');
+  parentCol.siblings('.drop-navigation__col').removeClass('active');
+});
+
+// -------------------------------------------------
+
+$('.btn-menu-open').click(function() {
+  $('.drop-navigation').toggleClass("active");
+  $(this).toggleClass("active");
+});
--- Belorechensk/script/owl-main.js
+++ Belorechensk/script/owl-main.js
@@ -10,6 +10,25 @@ $(document).ready(function () {
         autoplayTimeout:5000,
         nav: true,
         navText: ["<img src='../image/icons/arrow-left.svg'>","<img src='../image/icons/arrow-right.svg'>"],
+        responsive : {
+         // breakpoint from 0 up
+         0 : {
+            items:1,
+        },
+        // breakpoint from 480 up
+        768 : {
+            items: 2,
+        },
+        1050 : {
+            items: 2,
+
+        },
+        // breakpoint from 768 up
+        1439 : {
+            items: 2,
+        
+        }
+    }
     })
 });
 
@@ -26,7 +45,109 @@ $(document).ready(function () {
         autoplay:false,
         autoplayTimeout:5000,
         nav: true,
-        navText: ["<img src='../image/icons/arrow-left.svg'>","<img src='../image/icons/arrow-right.svg'>"]
+        autoHeight:true,
+        navText: ["<img src='../image/icons/arrow-left.svg'>","<img src='../image/icons/arrow-right.svg'>"],
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items:1,
+
+            },
+            // breakpoint from 480 up
+            768 : {
+                items: 2,
+                margin: 24,
+
+            },
+            1050 : {
+                items: 2,
+
+            },
+            // breakpoint from 768 up
+            1439 : {
+                items: 2,
+            
+            }
+        }
     })
 });
 
+// --------------------------------------------------
+
+$(document).ready(function () {
+    var owl = $('.owl-for-people');
+    owl.owlCarousel({
+        margin: 32,
+        center: false,
+        autoHeight: false,
+        loop:true,
+        items: 2,
+        autoplay:false,
+        autoplayTimeout:5000,
+        nav: true,
+        autoHeight:true,
+        navText: ["<img src='../image/icons/arrow-left.svg'>","<img src='../image/icons/arrow-right.svg'>"],
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items:1,
+
+            },
+            // breakpoint from 480 up
+            768 : {
+                items: 2,
+                margin: 24,
+
+            },
+            1050 : {
+                items: 2,
+
+            },
+            // breakpoint from 768 up
+            1439 : {
+                items: 2,
+            
+            }
+        }
+    })
+});
+
+// --------------------------------------------------
+
+$(document).ready(function () {
+    var owl = $('.owl-four-slider');
+    owl.owlCarousel({
+        margin: 32,
+        center: false,
+        autoHeight: false,
+        loop:false,
+        items: 4,
+        autoplay:false,
+        autoplayTimeout:5000,
+        nav: true,
+        autoHeight:true,
+        navText: ["<img src='../image/icons/arrow-left.svg'>","<img src='../image/icons/arrow-right.svg'>"],
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items:1,
+                margin:18
+
+            },
+            // breakpoint from 480 up
+            768 : {
+                items: 2,
+                margin:24
+            },
+            1050 : {
+                items: 3,
+
+            },
+            // breakpoint from 768 up
+            1439 : {
+                items: 4,
+            
+            }
+        }
+    })
+});
