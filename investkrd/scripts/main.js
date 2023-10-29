ec5b54f057ff3f74789d8ae5076c4603257f0303
Viktor Semikolenov <viktor.semikolenov@gmail.com>
2023-10-29 08:41:53 +0600

    add project

---
+++ investkrd/scripts/main.js
@@ -0,0 +1,71 @@
+$(function() {
+    var images = $(".first-screen__background-item");
+    var currentImageIndex = 0;
+  
+    function showNextImage() {
+      // Скрыть текущее изображение
+      images.eq(currentImageIndex).fadeOut();
+  
+      // Увеличить индекс для следующего изображения
+      currentImageIndex++;
+      if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+      }
+  
+      // Показать следующее изображение
+      images.eq(currentImageIndex).fadeIn();
+    }
+  
+    // Показать первое изображение при загрузке страницы
+    images.eq(0).show();
+  
+    // Запустить таймер для показа следующего изображения каждые 6 секунд
+    setInterval(showNextImage, 8000);
+  });
+
+
+// плавное появление текста для первого слайда
+
+$(document).ready(function() {
+    $('.play-icon').click(function() {
+      var investVideo = $('.video-invest')[0];
+  
+      $('.invest-video-poster, .play-icon').fadeOut(function() {
+        investVideo.play();
+      });
+    });
+  });
+// ---------------------
+
+// =----------------------------------------------------------
+$(document).ready(function() {
+    $('.project-info__icon').click(function() {
+      $(this).closest('.slider-item').toggleClass('active');
+    });
+  });
+// -------------------------------
+  $(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    var navigation = $('.navigation');
+    var offset = 150;
+  
+    if (scrollTop > offset) {
+      navigation.addClass('active');
+    } else {
+      navigation.removeClass('active');
+    }
+  });
+
+
+
+$(document).ready(function() {
+    $('.btn-search').click(function() {
+      $('.search-page').addClass('active');
+    });
+  });
+// -------------------------------
+  $(document).ready(function() {
+    $('.close-search').click(function() {
+      $('.search-page').removeClass('active');
+    });
+  });
---
+++ investkrd/scripts/owl-main.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    var owl = $('.owl-fs-info');
+    owl.owlCarousel({
+        margin: 2,
+        center: false,
+        autoHeight: false,
+        loop:true,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        nav: true
+    })
+});
+
+$(document).ready(function () {
+    var owl = $('.owl-fs-footer');
+    owl.owlCarousel({
+        margin: 32,
+        center: false,
+        autoHeight: false,
+        loop:true,
+        items: 5,
+        autoplay:false,
+        autoplayTimeout:6000,
+        nav: true
+    })
+});
+
+
+// ============================================
+
+
+// ---------------------------------------------------------
+
+$(document).ready(function () {
+    var owl = $('.owl-events-slider');
+    owl.owlCarousel({
+        margin: 32,
+        center: false,
+        autoHeight: false,
+        loop:true,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:6000,
+        nav: true,
+    })
+});
+// =----------------------------------------------------------
+
+$(document).ready(function () {
+    var owl = $('.owl-project');
+    owl.owlCarousel({
+        margin: 32,
+        center: false,
+        autoHeight: false,
+        loop:true,
+        items: 2,
+        autoplay:false,
+        autoplayTimeout:6000,
+        nav: true,
+    })
+});
+
+$(document).ready(function () {
+    var owl = $('.owl-footer-slider');
+    owl.owlCarousel({
+        margin: 32,
+        center: false,
+        autoHeight: false,
+        loop:true,
+        items: 5,
+        autoplay:false,
+        autoplayTimeout:6000,
+        nav: true,
+    })
+});
+
+
+
+
+
+
+
+
