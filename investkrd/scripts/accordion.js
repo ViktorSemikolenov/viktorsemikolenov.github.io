a6ccf7ea1dd7b75c9a76b7e43ab1434bc84343fa
Viktor Semikolenov <viktor.semikolenov@gmail.com>
2023-11-04 16:51:22 +0600

    add page

--- investkrd/scripts/accordion.js
+++ investkrd/scripts/accordion.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-    $('.accordion-header').click(function() {
-      // Сворачиваем все аккордеоны
-      $('.accordion-content').slideUp();
-      $('.accordion-item').removeClass('active');
-      
-      // Разворачиваем текущий аккордеон
+  $('.accordion-header').click(function() {
+    // Проверяем, не является ли текущий аккордеон уже развернутым
+    var isActive = $(this).parent('.accordion-item').hasClass('active');
+
+    // Сворачиваем все аккордеоны
+    $('.accordion-content').slideUp();
+    $('.accordion-item').removeClass('active');
+    
+    // Разворачиваем текущий аккордеон, если он не был активным
+    if (!isActive) {
       $(this).siblings('.accordion-content').slideDown();
       $(this).parent('.accordion-item').addClass('active');
-    });
+    }
   });
+});
+
+$(document).ready(function() {
+  $('.accordion-inside-header').click(function() {
+    // Проверяем, не является ли текущий аккордеон уже развернутым
+    var isActive = $(this).parent('.accordion-inside-item').hasClass('active');
+
+    // Сворачиваем все аккордеоны
+    $('.accordion-inside-content').slideUp();
+    $('.accordion-inside-item').removeClass('active');
+    
+    // Разворачиваем текущий аккордеон, если он не был активным
+    if (!isActive) {
+      $(this).siblings('.accordion-inside-content').slideDown();
+      $(this).parent('.accordion-inside-item').addClass('active');
+    }
+  });
+});
